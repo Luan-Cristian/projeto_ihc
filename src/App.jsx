a6be0b38086d63487c20b030d877128f4a202d7a
Luan-Cristian <luancristianbulhoess@gmail.com>
2026-06-08 17:14:49 -0300

    Corrige configuracao para github pages

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function AnimatedRoutes() {
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <AnimatedRoutes />
     </BrowserRouter>
   );
